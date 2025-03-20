4f0b788ea7cea563442e38921927abec397bc18c
ValerioMaiocchi <valerio.maiocchi94@gmail.com>
2025-03-20 15:31:25 +0100

    fix comments

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //represents an inventory slot.
-//It implements the IComparable interface to create its sorting system, giving each type of item a different value
 public class InventorySlot
 {
     public ItemBase item;
